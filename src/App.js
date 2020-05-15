66d31edfbf224c5f1aec37bb14787b0053338573
louis <louiscahyadi23@gmail.com>
2020-05-15 14:15:12 +0700

    front page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './res/bulma.css';
+
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
+
+      <section className="hero is-fullheight has-background-white">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title">
+            Achievements
+          </h1>
+        </div>
+      </div>
+      </section>
+
     </div>
   );
 }
