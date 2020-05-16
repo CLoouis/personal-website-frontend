86b7e1372caef698f83bcb3fe60e0c6390e437e1
louis <louiscahyadi23@gmail.com>
2020-05-16 14:57:59 +0700

    adding base pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import './res/bulma.css';
 
-import Home from './pages/home';
+import Home from './pages/Home/home';
+import Experience from './pages/Experience/experience';
+import Achievement from './pages/Achievement/achievement';
+import Project from './pages/Project/project';
 
 function App() {
   return (
     <div className="App">
       <Home />
-
-      <section className="hero is-fullheight has-background-white">
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title">
-            Achievements
-          </h1>
-        </div>
-      </div>
-      </section>
-
+      <Experience />
+      <Achievement />
+      <Project />
     </div>
   );
 }
---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../../res/bulma.css';
+import './home.css';    
+
+class Home extends React.Component {
+    render() {
+        return(
+            <section className="hero is-fullheight has-background-white-ter is-bold">
+                <div className="hero-body">
+                    <div className="container">
+                        <div className="columns is-vcentered reverse-columns is-centered">
+                            <div className="column has-text-centered">
+                                <p className="title is-size-3 is-size-4-mobile has-text-black">
+                                    I am Louis Cahyadi
+                                </p>
+                                <p className="subtitle is-size-5 is-size-6-mobile">
+                                    Computer Science Student<br></br>@ Institut Teknologi Bandung
+                                </p>
+                                <div className="columns has-text-centered is-centered is-mobile">
+                                    <div className="column is-narrow is-size-6 is-size-7-mobile">Achievements</div>
+                                    <div className="column is-narrow is-size-6 is-size-7-mobile">Experience</div>
+                                    <div className="column is-narrow is-size-6 is-size-7-mobile">Projects</div>
+                                </div>
+                            </div>
+                            <div className="column">
+                                <img className="my-picture center-horizontally" src={require('../../res/13517126-square.JPG')} alt="myself"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Home;
