a937d84b5fa488d02b6a9fa69dd22a31fdc47e25
Louis Cahyadi <louiscahyadi23@gmail.com>
2020-12-14 02:59:58 +0700

    Rewrite (#1)
    * create new homepage
    
    * update readme

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,35 +1,42 @@
 import React from 'react';
-import Fade from 'react-reveal'
-import Zoom from 'react-reveal/Zoom';
 
-import './home.css';    
+import './home.css';
 
 class Home extends React.Component {
     render() {
         return(
-            <section className="hero is-fullheight has-background-white-ter is-bold">
-                <div className="hero-body">
-                    <div className="container">
-                        <div className="columns is-vcentered reverse-columns is-centered">
-                            <div className="column has-text-centered">
-                                <Zoom>
-                                <p className="title is-size-3 is-size-4-mobile has-text-black">
-                                    I am Louis Cahyadi
-                                </p>
-                                <p className="subtitle is-size-5 is-size-6-mobile">
-                                    Computer Science Student<br></br>@ Institut Teknologi Bandung
-                                </p>
-                                </Zoom>
-                            </div>
-                            <div className="column">
-                                <Fade top>
-                                    <img className="my-picture center-horizontally" src={require('../../res/13517126-square.JPG')} alt="myself"/>
-                                </Fade>
-                            </div>
-                        </div>
-                    </div>
+            <div className="container center">
+                <div className="columns is-flex is-centered">
+                    <figure className="image is-text-centered">
+                        <img className="my-picture is-inline-block" src={require('../../res/louis-250.jpg')} alt="myself"/>
+                    </figure>
                 </div>
-            </section>
+                <div className="column has-text-centered">
+                    <p className="has-text-black name roboto-mono-light">
+                        Louis Cahyadi
+                    </p>
+                    <p className="subtitle title roboto-mono-light italic typewriter">
+                        Software Engineer
+                    </p>
+                </div>
+                <div className="columns social-media is-flex is-centered">
+                    <figure className="image is-32x32 is-text-centered my-icon">
+                        <a href="http://github.com/CLoouis" target="_blank" rel="noopener noreferrer">
+                            <img className="is-inline-block" src={require('../../res/github-icon.png')} alt="github"/>
+                        </a>
+                    </figure>
+                    <figure className="image is-32x32 is-text-centered my-icon">
+                        <a href="http://www.linkedin.com/in/louis-cahyadi" target="_blank" rel="noopener noreferrer">
+                            <img className="is-inline-block" src={require('../../res/linkedin-icon.png')} alt="linkedin"/>
+                        </a>
+                    </figure>
+                    <figure className="image is-32x32 is-text-centered my-icon">
+                        <a href="http://medium.com/@louiscahyadi23" target="_blank" rel="noopener noreferrer">
+                            <img className="is-inline-block" src={require('../../res/medium-icon.png')} alt="medium"/>
+                        </a>
+                    </figure>
+                </div>
+            </div>
         );
     }
 }
