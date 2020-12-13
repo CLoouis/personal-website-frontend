57baca54b85a006d18bebcdd979e1cd39d742f1c
louis <louiscahyadi23@gmail.com>
2020-12-14 02:56:26 +0700

    create new homepage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import './res/bulma.css';
-// import ReactPageScroller from 'react-page-scroller';
 
 import Home from './pages/Home/home';
-import Experience from './pages/Experience/experience';
-import AboutMe from './pages/AboutMe/AboutMe';
 
 function App() {
   return (
     <div className="App">
-      {/* <ReactPageScroller animationTimer={800}> */}
-        <Home />
-        <AboutMe />
-      {/* </ReactPageScroller> */}
-        <Experience />
+      <Home />
     </div>
   );
 }
